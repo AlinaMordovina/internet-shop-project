4d5ce879f60e82f7798ad80f9fd5681667e1fef4
Alina Mordovina <alina182@yandex.ru>
2024-02-10 04:13:19 +0400

    update

--- catalog/views.py
+++ catalog/views.py
@@ -10,6 +10,10 @@ def contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f"{name} {phone} {message}")
+
+        with open('contacts.txt', 'a', encoding='UTF-8') as file:
+            file.write(f'{name} {phone}: {message}\n')
+
+        print(f"{name} {phone}: {message}")
 
     return render(request, 'catalog/contacts.html')
