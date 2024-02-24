9620ee62831f0e2c60b6aa15335bb17871d72fa5
Alina Mordovina <alina182@yandex.ru>
2024-02-25 00:20:01 +0400

    homework_20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from catalog.views import contacts, homepage
 
+from catalog.apps import CatalogConfig
+from catalog.views import contacts, product_list, product_detail
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', homepage),
-    path('contacts/', contacts),
-]
+    path('', product_list, name='product_list'),
+    path('contacts/', contacts, name='contacts'),
+    path('products/<int:pk>/', product_detail, name='product_detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
